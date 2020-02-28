0c7083019d596c8689365a6af552f8f8207285b2
SebastianGarces <gsgarces1@gmail.com>
2020-02-27 22:12:35 -0500

    added nav, navlink and placeholders to setup initial routes - feature complete

---
+++ src/pages/IssueBoardPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const IssueBoardPage = () => {
+    return (
+        <div>
+            <h1>Issue Board</h1>
+        </div>
+    );
+};
+
+export default IssueBoardPage;
